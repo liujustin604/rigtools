3015a55d8397353032b95a3a0bad9da7716aca09
liujustin604 <liujustin604@gmail.com>
2024-08-30 16:02:05 -0400

    add basic extension disable functionality

--- extPayload.js
+++ extPayload.js
@@ -13,6 +13,8 @@ let x = html`
 <button type="button" id="refreshTabs">Refresh Tabs</button>
 <div id="tabsList"></div>
 
+
+<button type="button" id="refreshExtensions">Refresh Extensions</button>
 <div id="extensionsList"></div>
 </div>
 `;
@@ -21,19 +23,54 @@ document.body.appendChild(x);
 let manifest = chrome.runtime.getManifest();
 let permissions = manifest.permissions ?? [];
 
-if (permissions.includes("management")) {
-    chrome.management.getAll(handleExtensions)
+async function refreshList() {
+    let extList = document.getElementById("extensionsList");
+    extList.innerHTML = "";
+    let infoList = await getAll();
+    for (let extension of infoList) {
+        let [a, b]  = extItem(extension.name, extension.id, extension.enabled);
+        extList.appendChild(a);
+        extList.appendChild(b);
+    }
 }
 
-function handleExtensions(infoList) {
-    for (let extension of infoList) {
-        // TODO: Add functionality
-    }
+// We need to support MV2, which uses callbacks and does not support returning Promises like MV3 does
+function getAll() {
+    return new Promise((resolve, _reject) => chrome.management.getAll(resolve));
+}
+function setEnabled(id, enabled) {
+    return new Promise((resolve, _reject) => chrome.management.setEnabled(id, enabled, resolve));
+}
+function get(id) {
+    return new Promise((resolve, _reject) => chrome.management.get(id, resolve));
+}
+
+
+function extItem(title, id, enabled) {
+    let text = document.createElement("div");
+    text.textContent = `${title} (${id}, Currently ${enabled? "enabled" : "disabled"})`;
+    let btn = document.createElement("button");
+    btn.type = "button";
+    btn.id = `toggle${id}`;
+    btn.innerText = enabled ? "Disable": "Enable";
+    x.appendChild(text);
+    x.appendChild(btn);
+    btn.addEventListener("click", async () => {
+        await setEnabled(id, !enabled);
+        let ext = await get(id);
+        if (ext.enabled != enabled) {
+            alert("Success");
+            refreshList();
+        } else {
+            alert("Failed to disable extension");
+        }
+    });
+    return [text, btn];
 }
 
 function tabItem(title, url) {
     let x = document.createElement("div");
-    x.innerHTML = `${title} (${url})`;
+    x.textContent = `${title} (${url})`;
     return x;
 }
 let tabList = document.getElementById("tabsList");
@@ -51,5 +88,10 @@ function updateTabList() {
         tabList.appendChild(item);
     }));
 }
+
+if (permissions.includes("management")) {
+    await refreshList();
+}
+
 updateTabList();
 document.getElementById("refreshTabs").addEventListener("click", updateTabList);
