292a52347c00bccca34e598decbe2c9084b349aa
liujustin604 <liujustin604@gmail.com>
2024-08-29 22:14:28 -0400

    add tab display (cant change anything on the tabs yet)

--- extPayload.js
+++ extPayload.js
@@ -6,14 +6,32 @@ function Tab() {
 
 
 let x = html`
-<h1>Extension CodeExec</h1>
+<div><h1>Extension CodeExec</h1>
 <input type="text" id=""/>
 
-
-
+<h1>Tabs</h1>
+<div id="tabsList"></div>
+</div>
 `;
 document.body.appendChild(x);
 
-chrome.tabs.query({}, (x) => x.forEach((x) => {
-    
-}))
+function tabItem(title, url) {
+    let x = document.createElement("div");
+    x.innerHTML = `${title} (${url})`;
+    return x;
+}
+let tabList = document.getElementById("tabsList");
+function updateTabList() {
+    tabList.innerHTML = "";
+    chrome.tabs.query({}, (x) => x.forEach((x) => {
+        let title = x.title;
+        let url = x.url;
+        if (!title) {
+            title = "info not available";
+            url = "does this extension have the 'tabs' permission?"
+        }
+        let item = tabItem(title, url);
+        tabList.appendChild(item);
+    }));
+}
+updateTabList();
